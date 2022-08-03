70404782b587d042d653818277eb9319eb674258
secretMoi <win.moi@hotmail.com>
2022-08-03 22:38:16 +0200

    Add maquette for edit api page

--- src/app/pages/edit-api/edit-api.component.ts
+++ src/app/pages/edit-api/edit-api.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {MonitoredApi} from "../../models/monitored-api";
+import {ApiUrl} from "../../models/api-url";
 
 @Component({
   selector: 'app-edit-api',
@@ -8,6 +10,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class EditApiComponent implements OnInit {
 
+  api!: MonitoredApi;
 
   constructor(private route: ActivatedRoute, private router: Router) {
   }
@@ -16,6 +19,21 @@ export class EditApiComponent implements OnInit {
     this.router.events.subscribe((data) => {
       console.log(this.route.snapshot.params['name']);
     });
+
+    // if(this.route.snapshot.paramMap.get('name')) {
+    //
+    // } else {
+      this.api = new MonitoredApi();
+      this.api.apiUrls = [];
+      this.api.apiUrls.push(new ApiUrl());
+    // }
   }
 
+  addApiUrl() {
+    this.api.apiUrls.push(new ApiUrl());
+  }
+
+  removeApiUrl(index: number) {
+    this.api.apiUrls.splice(index, 1);
+  }
 }
