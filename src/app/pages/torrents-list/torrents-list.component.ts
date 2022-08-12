ba5e24f513b2841910a586ea4656ef3fee02b83b
secretMoi <win.moi@hotmail.com>
2022-08-12 21:52:07 +0200

    Beautify torrent table

--- src/app/pages/torrents-list/torrents-list.component.ts
+++ src/app/pages/torrents-list/torrents-list.component.ts
@@ -40,4 +40,51 @@ export class TorrentsListComponent implements OnInit {
     });
   }
 
+  getSizeToDisplay(size: number): string {
+    if (size > 1000000000) {
+      return Math.round(size / 10000000) / 100 + 'Go';
+    }
+
+    if (size > 1000000) {
+      return Math.round(size / 1000000) / 100 + 'Mo';
+    }
+
+    return size.toString();
+  }
+
+  displayState(state: string): string {
+    if (state === 'pausedUP') return 'assets/check.svg';
+    if (state === 'downloading') return 'assets/play.svg';
+    if (state === 'error') return 'assets/cancel.svg';
+
+    return 'assets/interrogation-mark.svg';
+  }
+
+  convertEta(seconds: number): string {
+    if(seconds == 8640000) return '0';
+
+    let d = Math.floor(seconds / (3600 * 24));
+    let h = Math.floor(seconds % (3600 * 24) / 3600);
+    let m = Math.floor(seconds % 3600 / 60);
+    let s = Math.floor(seconds % 60);
+
+    let dDisplay = d > 0 ? d + 'j' : "";
+    let hDisplay = h > 0 ? h + 'h' : "";
+    let mDisplay = m > 0 ? m + 'm' : "";
+    let sDisplay = s > 0 ? s + 's' : "";
+
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+  }
+
+  convertSpeed(speed: number): string {
+    if (speed > 1000000) {
+      return Math.round(speed / 1000000) / 100 + 'Mo/s';
+    }
+
+    if (speed > 1000) {
+      return Math.round(speed / 1000) / 100 + 'ko/s';
+    }
+
+    return speed.toString();
+  }
 }
