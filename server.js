4f30af59306403d259a2530a302b29bdfac53312
secretMoi <win.moi@hotmail.com>
2023-06-16 21:46:57 +0200

    Setup pipeline

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const port = 1212;
 const path = require('path');
 
-app.use(express.static('dist/'));
+app.use(express.static('_work/europium-frontend/europium-frontend/dist/'));
 
 app.get('*', function(req,res) {
-	console.log('get ' + req);
-	res.sendFile(path.resolve('dist/europium-frontend/index.html'));
+	res.sendFile(path.resolve('_work/europium-frontend/europium-frontend/dist/europium-frontend/index.html'));
 });
 
 app.listen(port, () => {
