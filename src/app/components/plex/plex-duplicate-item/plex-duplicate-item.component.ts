5f4ed5fb6c61dd7125966e1e4e9d326b7b7a8f30
secretMoi <win.moi@hotmail.com>
2023-07-25 07:58:47 +0200

    Fix duplicate picture lost

--- src/app/components/plex/plex-duplicate-item/plex-duplicate-item.component.ts
+++ src/app/components/plex/plex-duplicate-item/plex-duplicate-item.component.ts
@@ -24,6 +24,10 @@ export class PlexDuplicateItemComponent extends BaseComponent implements AfterVi
 	}
 
 	ngAfterViewInit() {
+		this.getThumbnail();
+	}
+
+	getThumbnail() {
 		this._plexService.getThumbnail({
 				size: this.duplicateChild.nativeElement.offsetWidth,
 				isArt: true,
