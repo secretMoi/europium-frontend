e796dc0c39b539042039e7c84d43cab534f2bb42
secretMoi <win.moi@hotmail.com>
2023-08-01 13:43:09 +0200

    Clean up

--- src/app/components/ui/filter-container/filter-container.component.ts
+++ src/app/components/ui/filter-container/filter-container.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter-container',
   templateUrl: './filter-container.component.html',
   styleUrls: ['./filter-container.component.scss']
 })
-export class FilterContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FilterContainerComponent {
 }
