676354d57ed2b5b48ef633205c8c6a6bb25e4571
Lampirou <win.moi@hotmail.com>
2023-08-19 23:55:30 +0200

    Add refresh token basic

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,25 +1,56 @@
 import {Injectable} from "@angular/core";
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {AuthService} from "../service/auth.service";
-import {Observable} from "rxjs";
+import {catchError, Observable, switchMap, throwError} from "rxjs";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 	constructor(private authService: AuthService) {}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		const token = this.authService.getToken();
+		const token = this.authService.token;
 
 		if (token) {
-			const authRequest = request.clone({
+			request = request.clone({
 				setHeaders: {
 					Authorization: `Bearer ${token}`,
 				},
 			});
-
-			return next.handle(authRequest);
 		}
 
-		return next.handle(request);
+		return next.handle(request).pipe(
+			catchError(error => {
+				if (error.status === 401) {
+					// Token expired, try to refresh it
+					return this.authService.refreshTokens().pipe(
+						switchMap(() => {
+							// Retry the original request with the new token
+							const newToken = this.authService.token;
+							if (newToken) {
+								request = request.clone({
+									setHeaders: {
+										Authorization: `Bearer ${newToken}`,
+									},
+								});
+								return next.handle(request);
+							} else {
+								// Refreshed token is also invalid, redirect to login
+								this.authService.logout();
+								// Redirect or handle the situation in your application
+								return throwError('Refreshed token is invalid');
+							}
+						}),
+						catchError(() => {
+							// Unable to refresh token, logout user
+							this.authService.logout();
+							// Redirect or handle the situation in your application
+							return throwError('Unable to refresh token');
+						})
+					);
+				} else {
+					return throwError(error);
+				}
+			})
+		);
 	}
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,14 +2,24 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, tap} from "rxjs";
 import {environment} from "../../environments/environment";
+import {Router} from "@angular/router";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
 	private tokenKey = 'europium_token';
+	private refreshTokenKey = 'europium_refresh_token';
 
-	constructor(private http: HttpClient) {}
+	get token(): string | null {
+		return localStorage.getItem(this.tokenKey);
+	}
+
+	get refreshToken(): string | null {
+		return localStorage.getItem(this.refreshTokenKey);
+	}
+
+	constructor(private http: HttpClient, private router: Router) {}
 
 	login(username: string, password: string): Observable<any> {
 		return this.http.post(environment.backendUrl + `/auth/login`, {username, password})
@@ -17,15 +27,31 @@ export class AuthService {
 				tap((response: any) => {
 					if (response.token)
 						localStorage.setItem(this.tokenKey, response.token);
+					if (response.refreshToken)
+						localStorage.setItem(this.refreshTokenKey, response.refreshToken);
 				})
 			);
 	}
 
-	getToken(): string | null {
-		return localStorage.getItem(this.tokenKey);
+	refreshTokens(): Observable<any> {
+		const refreshToken = this.refreshToken;
+		if (!refreshToken) {
+			// Handle error: No refresh token available
+		}
+
+		return this.http.post(environment.backendUrl + `/auth/refresh`, { refreshToken }).pipe(
+			tap((response: any) => {
+				if (response.token) {
+					localStorage.setItem(this.tokenKey, response.token);
+				}
+			})
+		);
 	}
 
 	logout() {
 		localStorage.removeItem(this.tokenKey);
+		localStorage.removeItem(this.refreshTokenKey);
+
+		this.router.navigate(['/login']);
 	}
 }
